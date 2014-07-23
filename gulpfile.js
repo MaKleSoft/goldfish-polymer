5f3cf37548124a4309b4647a5b6c62ebf18d1903
Martin Kleinschrodt <martin@maklesoft.com>
2014-07-23 16:02:25 +0200

    Add gulp serve task for easily serving the app locally

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require("gulp"),
     less = require("gulp-less"),
     watch = require("gulp-watch"),
-    argv = require("yargs").argv;
+    argv = require("yargs").argv,
+    connect = require("connect");
 
 // Compile all less files in place
 gulp.task("less", function () {
@@ -15,4 +16,15 @@ gulp.task("less", function () {
             .pipe(less())
             .pipe(gulp.dest("./src"));
     }
+});
+
+// Start a server listening on localhost port 8000
+gulp.task("serve", function() {
+    // The port can be specified via the --port flat
+    // e.g. `gulp serve --port 8080
+    var port = argv.port || 8000;
+    connect.createServer(
+        connect.static(__dirname)
+    ).listen(port);
+    console.log("Serving app on port " + port + ". If you want a different port use the --port flag.");
 });
