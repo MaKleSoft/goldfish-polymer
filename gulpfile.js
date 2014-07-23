6169c7df0e704e9cce614bd561e42f0da2bd4bdf
Martin Kleinschrodt <martin@maklesoft.com>
2014-07-23 16:53:05 +0200

    Add deploy task

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,13 @@ var gulp = require("gulp"),
     less = require("gulp-less"),
     watch = require("gulp-watch"),
     argv = require("yargs").argv,
-    connect = require("connect");
+    connect = require("connect"),
+    vulcan = require("vulcanize"),
+    Q = require("q"),
+    rmdir = require("rimraf"),
+    ncp = require("ncp").ncp,
+    path = require("path"),
+    mkdirp = require("mkdirp");
 
 // Compile all less files in place
 gulp.task("less", function () {
@@ -27,4 +33,51 @@ gulp.task("serve", function() {
         connect.static(__dirname)
     ).listen(port);
     console.log("Serving app on port " + port + ". If you want a different port use the --port flag.");
+});
+
+function build(dest) {
+    var deferred = Q.defer();
+
+    vulcan.setOptions({
+        verbose: true,
+        inline: true,
+        csp: true,
+        input: "index.html",
+        output: path.join(dest, "index.html")
+    }, function(err) {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        vulcan.processDocument();
+        deferred.resolve();
+    });
+
+    return deferred.promise;
+}
+
+// Deploy a minified/built version of the app to a given destination folder
+gulp.task("deploy", function() {
+    var dest = argv.dest || "deploy";
+    console.log("Deploying app to " + path.resolve(process.cwd(), dest));
+    Q(function() {
+        console.log("Cleaning up existing target folder...");
+        return Q.nfcall(rmdir, dest);
+    })
+    .then(function() {
+        console.log("Creating target folder...");
+        return Q.nfcall(mkdirp, dest);
+    })
+    .then(function() {
+        console.log("Building source...");
+        return build(dest);
+    })
+    .then(function() {
+        console.log("Copying assets...");
+        return Q.nfcall(ncp, "assets", path.join(dest, "assets"));
+    })
+    .then(function() {
+        console.log("Done!");
+        process.exit(1);
+    });
 });
