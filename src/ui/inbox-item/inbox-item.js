dbce1c0bfe14d3e45c6659fd9184b14b16684b5a
Martin Kleinschrodt <martin@maklesoft.com>
2014-07-25 12:30:16 +0200

    Use filter expression instead of property accessor for displaying time

---
+++ src/ui/inbox-item/inbox-item.js
@@ -0,0 +1,25 @@
+Polymer("goldfish-inbox-item", {
+    shortTime: function(val) {
+        var dt = new Date().getTime() - new Date(val),
+            s = Math.floor(dt / 1000),
+            mi = Math.floor(s / 60),
+            h = Math.floor(mi / 60),
+            d = Math.floor(h / 24),
+            mo = Math.floor(d / 30), // Not 100% precise but should be sufficient,
+            y = Math.floor(mo / 12);
+
+        if (mi < 1) {
+            return s + "s";
+        } else if (h < 1) {
+            return mi + "mi";
+        } else if (d < 1) {
+            return h + "h";
+        } else if (mo < 1) {
+            return d + "d";
+        } else if (y < 1) {
+            return mo + "m";
+        } else {
+            return y + "y";
+        }
+    }
+});
